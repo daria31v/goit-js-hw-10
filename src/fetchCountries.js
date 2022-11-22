c1892bc0b7e812c76f52ca032822605d970e31c5
Daria Vdovichenko <darya.vdovichenko86@gmail.com>
2022-11-22 20:51:39 +0100

    комміт

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,20 +1,21 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
-const BASE_URL = 'https://restcountries.com/v2/all/'
+const BASE_URL = 'https://restcountries.com/v3.1/name/'
 const fields = 'name,capital,population,languages,flags'
 
 
 export default function fetchCountries(name) {
-    console.log(name)
+    
   return fetch(`${BASE_URL}${name}?fields=${fields}`)
        .then(response => {
-        //    console.log(response)
+           console.log(response)
            if (!response.ok) {
                Notify.failure("Oops, there is no country with that name")
                throw new Error(response.statusText)
-               
-        }
+           }
            return response.json()
-       })
+           
+       }).then(data => console.log(data))
+        .catch (err => console.error(err))
       
 }
--- src/index.js
+++ src/index.js
@@ -3,11 +3,8 @@ import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import debounce from 'lodash.debounce';
 import fetchCountries from './fetchCountries'
 
-// console.log(Notiflix)
-// import fetchCountries from './fetchCountries';
+
 const DEBOUNCE_DELAY = 300;
-
-
 
 const searchBox = document.querySelector('#search-box');
 const countryList = document.querySelector('.country-list');
@@ -16,23 +13,31 @@ const countryInfoCard = document.querySelector('.country-info');
 searchBox.addEventListener('input', debounce(onSearch, DEBOUNCE_DELAY))
  
 function onSearch(evt) {
-    const inputUser = evt.target.value.trim()
-    console.log(inputUser)
+    console.log('INPUT')
+    const name = evt.target.value
+        // .trim().toLowerCase();
+    console.log(name)
+    // const filteredItems = data.filter(t =>t..toLowerCase().includes(filter)
+    // );
+
+    // const listItemsMarkup = createCountryCard(filteredItems);
+    // countryInfoCard.innerHTML = listItemsMarkup
    
-    fetchCountries(inputUser)
-        .then(data => {
-            if (data.length > 10) {
-                Notify.info("Too many matches found. Please enter a more specific name.");
-            } else if (data.length <= 10 & data.length > 1) {
-                createCountryList(data);
-            } else if (data.length = 1) {
-                createCountryCard(data);
-            }
-        }).catch(err => console.log(err))
-    // if (!evt.textContent) {
-    //     clearList();
-    //     clearCard();
-    //     }
+    fetchCountries(name)
+//         .then(data => {
+//             if (data.length > 10) {
+//                 Notify.info("Too many matches found. Please enter a more specific name.");
+//             } else if (data.length <= 10 & data.length > 1) {
+//                 createCountryList(data);
+//             } else if (data.length = 1) {
+//                 createCountryCard(data);
+//             }
+//         }).catch(err => console.log(err))
+//     if (!evt.textContent) {
+//         clearList();
+//         clearCard();
+            // return
+//         }
 
 }
 
@@ -42,31 +47,31 @@ function onSearch(evt) {
 
 
 
-function createCountryCard(arr) {
-    const markupCard = arr.map(({ name, capital, population, languages, flags}) => 
-      `<div class="card-county">
-      <p class="name-country">${name.official}</p>
-      <p class="info-country">Capital: ${capital}</p>
-      <p class="info-country">Population: ${population}</p>
-      <p class="info-country">Languages: ${languages}</p>
-      <img src="${flags.svg}" alt="flag coutry" />
-      </div>`).join('');
+// function createCountryCard(arr) {
+//     const markupCard = arr.map(({ name, capital, population, languages, flags}) => 
+//       `<div class="card-county">
+//       <p class="name-country">${name.official}</p>
+//       <p class="info-country">Capital: ${capital}</p>
+//       <p class="info-country">Population: ${population}</p>
+//       <p class="info-country">Languages: ${Object.values(languages)}</p>
+//       <img src="${flags.svg}" alt="flag flag ${name}" />
+//       </div>`).join('');
 
-    countryInfoCard.innerHTML = markupCard;
+//     countryInfoCard.innerHTML = markupCard;
 
-}
+// }
 
 
 function createCountryList(arr) {
-    const markupList = arr.map(({ flags, name }) =>
+    const markupList = arr.map((item =>
         `<li class="list-county">
-      <img src="${flags.svg}" alt="flag coutry" width="50" height="30" />
-      <p class="name-country">${name}</p>
-      </li>`).join('');
- 
+      <img src="${item.flags.svg}" alt="flag ${item.name}" width="50" height="30" />
+      <p class="name-country">${item.name.official}</p>
+      </li>`).join(''))
+    
     countryList.style.listStyle = 'none';
     countryList.innerHTML = markupList;
-
+    
 }
 
 
